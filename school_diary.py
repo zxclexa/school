57e467407d6bb515c59f845df36ee1111b0f81e6
Tea With Urgant <TeaWithUrgant@gmail.com>
2024-11-29 23:07:58 +0300

    add file

---
+++ school_diary.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+class visiting:
+    def __init__(self,name,date_of_birth,grade):
+        self.name=name
+        self.date_of_birth=date_of_birth
+        self.grade=grade
+
+
+        pass
+
+class rates:
+    def __init__(self,name,subject):
+        self.name=name
+        self.subject=subject
+
+class students:
+    def __init__(self,name):
+        self.name=name
+    pass
